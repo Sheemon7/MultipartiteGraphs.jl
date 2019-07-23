f1beab540cbd17f2f341c4ad1e2f2b3b110e355d
Simon Mandlik <simon.mandlik@gmail.com>
2019-07-23 10:41:49 +0200

    Metagraphs wrapper

--- src/MultipartiteGraphs.jl
+++ src/MultipartiteGraphs.jl
@@ -4,10 +4,16 @@ using LightGraphs
 using MetaGraphs
 
 import LightGraphs:
-    ne, nv, vertices, edges, has_edges, has_vertex,
-    is_directed, inneighbors, outneighbors, all_neighbors,
+    ne, nv, vertices, edges, has_edge, has_vertex,
+    is_directed, inneighbors, outneighbors,
     dst, src,
     add_vertex!, add_edge!
+
+import MetaGraphs:
+    props, get_prop,
+    set_props!, set_prop!,
+    rem_prop!, has_prop,
+    clear_props!
 
 import GraphPlot:
     gplot
@@ -21,10 +27,17 @@ end
 MultipartiteGraph() = MultipartiteGraph(MetaGraph(Graph()))
 
 for f in [:ne, :nv, :vertices, :edges, :has_edge, :has_vertex,
-          :is_directed, :inneighbors, :outneighbors, :all_neighbors,
-          :dst, :src, :gplot]
+          :inneighbors, :outneighbors,
+          :dst, :src, :gplot,
+          :props, :get_prop,
+          :set_props!, :set_prop!,
+          :rem_prop!, :has_prop,
+          :clear_props!
+         ]
     @eval @inline $f(g::MultipartiteGraph, args...; kwargs...) = $f(g.mg, args...; kwargs...)
 end
+is_directed(::MultipartiteGraph{T}) where T = false
+is_directed(::Type{MultipartiteGraph{T}}) where T = false
 
 function add_vertex!(g::MultipartiteGraph, partite::AbstractString;
                      props::PropDict=Dict{Symbol, Any}())
@@ -38,7 +51,10 @@ function add_edge!(g::MultipartiteGraph{T}, u::T, v::T, relation::AbstractString
     set_props!(g.mg, u, v, merge(props, Dict(:relation_type => relation)))
 end
 
-relation_edges(g::MultipartiteGraph, relation::AbstractString) = filter_edges(g.mg, :relation_type, relation)
+nv(g::MultipartiteGraph, partite::AbstractString) = length(collect(vertices(g, partite)))
+ne(g::MultipartiteGraph, relation::AbstractString) = length(collect(edges(g, relation)))
+vertices(g::MultipartiteGraph, partite::AbstractString) = filter_vertices(g.mg, :partite, partite)
+edges(g::MultipartiteGraph, relation::AbstractString) = filter_edges(g.mg, :relation_type, relation)
 
 export MultipartiteGraph
 
